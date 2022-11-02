545da096b76641a94fe35729080f7918555c6721
killersmath <maxsuelfilomeno@gmail.com>
2022-11-02 02:32:35 -0300

    first commit - mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from "./src/components/Loading";
+import { SignIn } from "./src/screens/SignIn";
+
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar  
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {
+        !fontsLoaded ? <Loading /> : <SignIn />
+      }
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
