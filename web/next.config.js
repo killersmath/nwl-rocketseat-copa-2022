975e212507dc09e64f258c2521f7d6f90f200134
killersmath <maxsuelfilomeno@gmail.com>
2022-11-04 00:00:07 -0300

    chore: update eslint and gitignore rules

--- web/next.config.js
+++ web/next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config, options) => {
-    if (options.dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      }
-    }
-    return config
-  },
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack: (config, options) => {
+        if (options.dev) {
+            config.watchOptions = {
+                poll: 1000,
+                aggregateTimeout: 300,
+            };
+        }
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
